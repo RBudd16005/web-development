153455770a353fcebe0f94643edc4b6bb93bac1a
Ryan <bud16005@byui.edu>
2019-11-16 11:49:05 -0700

    errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-//const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
