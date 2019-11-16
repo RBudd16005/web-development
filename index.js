bd5f8fd9a78a442f1c7b4693ef6c8b58d8aea934
Ryan <bud16005@byui.edu>
2019-11-16 11:47:59 -0700

    errors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/postal'))
+  //.post('/results', urlencodedParser, function(req, res){ console.log(req.body); })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
